5fff50f6e4fcde63903c62cde969b2faaba88b27
Yago Fontenla Seco <yago.fontenla.seco@usc.es>
2023-09-06 08:45:19 +0200

    update script

--- service/deploy.sh
+++ service/deploy.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 # Activate virtual environment
+cd /home/citius/c-4pm || exit
 source venv/bin/activate
 
 # Terminate all rasa and gunicorn processes running
@@ -8,9 +11,10 @@ pkill gunicorn
 # Launch rasa actions server
 cd actions || exit
 rasa run actions &
-cd ..
 
+# Launch rasa server
+cd ..
 rasa run --enable-api &
 
 # Deploy dash app with rasa bot
-gunicorn wsgi:application -b :8008 --timeout 240
+gunicorn wsgi:application -b :8000 --timeout 240
